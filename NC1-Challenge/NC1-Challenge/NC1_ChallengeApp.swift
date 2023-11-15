1ab51c822ea3341d3900539a6d1e21d7b62ec1c7
Domenico <148193907+Dom-mennillo2@users.noreply.github.com>
2023-11-15 10:21:20 +0100

    Initial Start

---
+++ NC1-Challenge/NC1-Challenge/NC1_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NC1_ChallengeApp.swift
+//  NC1-Challenge
+//
+//  Created by Domenico Mennillo on 15/11/23.
+//
+
+import SwiftUI
+
+@main
+struct NC1_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
